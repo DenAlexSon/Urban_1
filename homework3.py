01cec1d824bbe1e454a9245d1504077bb5fe9759
DenAlexSon <den.xxx@list.ru>
2024-07-03 19:12:48 +0300

    Urban

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Denis'
+print('Name: '+(name))
+age = 38
+print('Age: '+(str(age)))
+new_age = 39
+print('New age: '+(str(new_age)))
+is_student = True
+print('Is Student: '+(str(is_student)))
---
+++ task4.py
@@ -0,0 +1,7 @@
+#4th program
+a=13.42
+b=42.13
+print((int(a//1)),(int(a%1*101)))
+print((int(b//1)),(int(b%1*101)))
+
+print(((int(a//1))==(int(b//1))or((int(a%1*101))==(b%1*101)))or((int(a//1))==(int(b%1*101)))or((int(a%1*101))==(int(b%1*101))))
